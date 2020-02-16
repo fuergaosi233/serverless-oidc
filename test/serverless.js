2e6e07cf249a5a3bba7d8b37ae6636fc937e314e
ivy <xieyang@dodora.cn>
2020-02-16 21:01:57 +0800

    typo

--- test/serverless.js
+++ test/serverless.js
@@ -2,8 +2,8 @@ const { Component } = require('@serverless/core')
 
 class myFirstComponent extends Component {
     async default() {
-      const ServerlessOIDC = await this.load('../')
-      const oidcUrl = await ServerlessOIDC({
+      const serverlessOIDC = await this.load('../')
+      const oidcUrl = await serverlessOIDC({
         client_id: '5d3ab8f461ec9c8bbbb4fd2b',
         redirect_uri: 'http://localhost:4577/redirect',
         domain: 'rabbit.authing.cn',
@@ -18,7 +18,7 @@ class myFirstComponent extends Component {
       console.log(oidcUrl);
 
       // 2. 用户登录完成后会在回调 URL 中得到 code，将 Code 作为填到下列参数中：
-      const code2Token = await ServerlessOIDC.getTokenByCode({
+      const code2Token = await serverlessOIDC.getTokenByCode({
         code: 'tTSXi~rc_3LtocXC0vMAYE_yVIj',
         client_secret: 'd767fe4dc4c3f48f0697f0ccbb00dd5c',
         grant_type: 'authorization_code',
@@ -26,15 +26,15 @@ class myFirstComponent extends Component {
       console.log(code2Token);
 
       // 3. 检验 token 合法性 
-      const checkToken = await ServerlessOIDC.checkToken(code2Token.id_token);
+      const checkToken = await serverlessOIDC.checkToken(code2Token.id_token);
       console.log(checkToken);
 
       // 4. 使用 token 换取用户信息
-      const userInfo = await ServerlessOIDC.getUserInfoByAccessToken(code2Token.access_token);
+      const userInfo = await serverlessOIDC.getUserInfoByAccessToken(code2Token.access_token);
       console.log(userInfo);
 
       // 5. 刷新 token
-      const refreshToken = await ServerlessOIDC.refreshToken({
+      const refreshToken = await serverlessOIDC.refreshToken({
         client_secret: 'd767fe4dc4c3f48f0697f0ccbb00dd5c',
         refresh_token: code2Token.refresh_token,
       }); 
